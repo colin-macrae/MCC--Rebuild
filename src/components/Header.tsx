6040e2cd485516117d769fed45ffcee3abc8432f
Colin Macrae <csmacrae@gmail.com>
2024-04-16 18:18:28 -0700

    additional nav fixes & adjustments for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,84 +9,87 @@ import { useState } from "react";
 export function Header() {
   const [servicesOnHover, setServicesOnHover] = useState(false);
 
-    const navigate = useNavigate();
-  
+  const navigate = useNavigate();
 
   return (
     <>
-      <div className="HEADER flex bg-[#005067] sticky top-0 z-10">
-        <div className="HEAD-LEFT basis-1/3 CONTENTS flex items-center justify-center">
-          <div className="LOGO flex h-36 object-contain">
+      <div className="HEADER bg-[#005067] sticky top-0 z-10">
+        <div className="header-container flex items-center justify-between">
+          <div className="LOGO flex h-36">
             <img className="self-start mt-6" src={coloredLogo}></img>
           </div>
-        </div>
-        <div className="HEAD-CENTER flex basis-1/3 justify-center items-center text-white font-bold">
-          <Link to="/MCC-Rebuild/">
-            <div className="mb-10 mr-12">HOME</div>
-          </Link>
-          <Link to="/MCC-Rebuild/about">
-            <div className="mb-10 mr-12">ABOUT</div>
-          </Link>
-          <Link to="/MCC-Rebuild/employment">
-            <div className="mb-10 mr-12">EMPLOYMENT</div>
-          </Link>
-          <div
-            onMouseEnter={() => setServicesOnHover(true)}
-            onMouseLeave={() => setServicesOnHover(false)}
-            className="SERVICES relative mb-10 mr-12"
-          >
-            <div className="services w-[5.625rem]">
-              SERVICES &#9660;
-              {servicesOnHover && (
-                <div className="DROP-DOWN absolute top-6 left-0 h-max bg-[#008cb3] flex flex-col w-60 rounded">
-                  <button
-                    className="text-m pt-2 px-2 text-left"
-                    onClick={() => navigate("/MCC-Rebuild/Commercial-Cleaning")}
-                  >
-                    Commercial Cleaning
-                  </button>
-                  <button
-                    className="text-m pt-2 px-2 text-left"
-                    onClick={() => navigate("/MCC-Rebuild/Janitorial-Services")}
-                  >
-                    Janitorial Services
-                  </button>
-                  <button
-                    className="text-m pt-2 px-2 text-left"
-                    onClick={() => navigate("/MCC-Rebuild/Carpet-Cleaning")}
-                  >
-                    Commercial Carpet Cleaning
-                  </button>
-                  <button
-                    className="text-m pt-2 px-2 pb-2 text-left"
-                    onClick={() => navigate("/MCC-Rebuild/Office-Cleaning")}
-                  >
-                    Office Cleaning
-                  </button>
-                </div>
-              )}
-            </div>
-          </div>
-
-          <Link to="/MCC-Rebuild/contact">
-            <div className="mb-10 mr-12">CONTACT</div>
-          </Link>
-        </div>
-        <div className="HEAD-RIGHT basis-1/3 flex justify-center items-center mb-20 text-white font-bold leading-5">
-          <div className=" EMAIL-US flex justify-between items-baseline text-white font-bold leading-5 mr-14">
+          <div className="HEAD-RIGHT flex items-center mb-10 text-white font-bold">
             <button className="flex leading-5">
               <span className="pr-1.5">
                 <HiOutlineMail size={20} />
               </span>{" "}
               EMAIL US
             </button>
+            <button className="text-white text-center h-12 px-4 self-center bg-[#F58800] rounded font-bold mx-5">
+              CALL US NOW
+            </button>
+            <button className="flex text-white">
+              <FaLinkedin size={40} />
+            </button>
           </div>
-          <button className="text-white text-center h-12 px-4 self-center bg-[#F58800] rounded font-bold">
-            CALL US NOW
-          </button>
-          <button className="flex text-white ml-14">
-            <FaLinkedin size={40} />
-          </button>
+        </div>
+        <div className="navbar flex justify-center pb-4">
+          <div className="HEAD-CENTER flex text-white font-bold">
+            <Link className="nav-item" to="/MCC-Rebuild/">
+              <div>HOME</div>
+            </Link>
+            <Link className="nav-item" to="/MCC-Rebuild/about">
+              <div>ABOUT</div>
+            </Link>
+            <Link className="nav-item" to="/MCC-Rebuild/employment">
+              <div>EMPLOYMENT</div>
+            </Link>
+            <div
+              onMouseEnter={() => setServicesOnHover(true)}
+              onMouseLeave={() => setServicesOnHover(false)}
+              className="SERVICES relative nav-item"
+            >
+              <div className="services w-[5.625rem]">
+                SERVICES &#9660;
+                {servicesOnHover && (
+                  <div className="DROP-DOWN absolute top-6 left-0 h-max bg-[#008cb3] flex flex-col w-60 rounded">
+                    <button
+                      className="text-m pt-2 px-2 text-left"
+                      onClick={() =>
+                        navigate("/MCC-Rebuild/Commercial-Cleaning")
+                      }
+                    >
+                      Commercial Cleaning
+                    </button>
+                    <button
+                      className="text-m pt-2 px-2 text-left"
+                      onClick={() =>
+                        navigate("/MCC-Rebuild/Janitorial-Services")
+                      }
+                    >
+                      Janitorial Services
+                    </button>
+                    <button
+                      className="text-m pt-2 px-2 text-left"
+                      onClick={() => navigate("/MCC-Rebuild/Carpet-Cleaning")}
+                    >
+                      Commercial Carpet Cleaning
+                    </button>
+                    <button
+                      className="text-m pt-2 px-2 pb-2 text-left"
+                      onClick={() => navigate("/MCC-Rebuild/Office-Cleaning")}
+                    >
+                      Office Cleaning
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+
+            <Link className="nav-item" to="/MCC-Rebuild/contact">
+              <div>CONTACT</div>
+            </Link>
+          </div>
         </div>
       </div>
       <Outlet />
