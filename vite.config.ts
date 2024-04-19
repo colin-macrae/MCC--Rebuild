0734ac688ebbc2c6adf71067d29ed5ba87e8c2dd
Colin Macrae <csmacrae@gmail.com>
2024-04-19 11:03:14 -0700

    test 9

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MCC--Rebuild/",
-  build: {
-    outDir: "dist", // Make sure this matches what Netlify expects
-  },
+  base: "/",
   plugins: [react()],
   publicDir: "public",
 });
