a33cdbab89e4bf03aeec0eb3f06a337f5064ff9e
James Aguirre <jamesaguirre98@gmail.com>
2024-04-19 01:31:06 -0700

    top section of contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,12 +3,22 @@ import "./Employment.css";
 import "./Contact.css";
 
 export default function Contact(): JSX.Element {
-    return (
-      <div>
-        I am the contact page!
+  return (
+    <div className="background-image">
+      <section className="about-heading-section">
+        <div className="about-heading-text">
+          <h2>Cleaning Done Right</h2>
+          <p>
+            Experience why we're the preferred corporate cleaning company in the New
+            England area. Contact us today to get a quote on your services and discover
+            what makes Mass Commercial Cleaning a trusted janitorial partner.
+          </p>
+          <button className="btn">CONTACT US &rsaquo;</button>
+        </div>
+      </section>
+      <section className="form-container">
         <ContactForm />
-      </div>
-    );
-  
+      </section>
+    </div>
+  );
 }
-
