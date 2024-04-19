17219a9f9a7ac360af0bb5b930554a802d6f3a36
Colin Macrae <csmacrae@gmail.com>
2024-04-18 18:30:19 -0700

    debugging test #3

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/MCC-Rebuild/",
+  base: "/MCC--Rebuild/",
   plugins: [react()],
   publicDir: 'public'
 })
