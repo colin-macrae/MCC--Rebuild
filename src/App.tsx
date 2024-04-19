60885f1697cb747829530124df617a899f724dcb
Colin Macrae <csmacrae@gmail.com>
2024-04-18 17:17:00 -0700

    debugging test: change routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,28 +5,27 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Employment from "./pages/Employment";
 import CarpetCleaning from "./pages/Services-Pages/Carpet-Cleaning";
-import CommericalCleaning from "./pages/Services-Pages/Commercial-Cleaning";
+import CommercialCleaning from "./pages/Services-Pages/Commercial-Cleaning";
 import JanitorialServices from "./pages/Services-Pages/Janitorial-Services";
 import OfficeCleaning from "./pages/Services-Pages/Office-Cleaning";
 import Footer from "./components/Footer";    
     
-export default function App(){
-
-    return(
-          <>
+export default function App() {
+    return (
+        <>
             <Routes>
-                <Route path="/MCC-Rebuild/" element={<Header />}>
-                    <Route index element={<Landing/>}/>
-                    <Route path="/MCC-Rebuild/about" element={<About/>}/>
-                    <Route path="/MCC-Rebuild/contact" element={<Contact/>}/>
-                    <Route path="/MCC-Rebuild/employment" element={<Employment/>}/>
-                    <Route path="/MCC-Rebuild/Commercial-Cleaning" element={<CommericalCleaning/>}/>
-                    <Route path="/MCC-Rebuild/Carpet-Cleaning" element={<CarpetCleaning/>}/>
-                    <Route path="/MCC-Rebuild/Janitorial-Services" element={<JanitorialServices/>}/>
-                    <Route path="/MCC-Rebuild/Office-Cleaning" element={<OfficeCleaning/>}/>
+                <Route path="/" element={<Header />}>
+                    <Route index element={<Landing />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="contact" element={<Contact />} />
+                    <Route path="employment" element={<Employment />} />
+                    <Route path="Commercial-Cleaning" element={<CommercialCleaning />} />
+                    <Route path="Carpet-Cleaning" element={<CarpetCleaning />} />
+                    <Route path="Janitorial-Services" element={<JanitorialServices />} />
+                    <Route path="Office-Cleaning" element={<OfficeCleaning />} />
                 </Route>
             </Routes>
-        <Footer/>
+            <Footer />
         </>
     );
 }
