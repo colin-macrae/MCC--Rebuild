f015dd937820a00403b55287501087a69ebbc57e
Colin Macrae <csmacrae@gmail.com>
2024-04-19 11:06:42 -0700

    test 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-// import { Route, Routes } from "react-router-dom";
-// import { Header } from "./components/Header";
-// import Landing from "./pages/Landing";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Employment from "./pages/Employment";
-// import CarpetCleaning from "./pages/Services-Pages/Carpet-Cleaning";
-// import CommercialCleaning from "./pages/Services-Pages/Commercial-Cleaning";
-// import JanitorialServices from "./pages/Services-Pages/Janitorial-Services";
-// import OfficeCleaning from "./pages/Services-Pages/Office-Cleaning";
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Employment from "./pages/Employment";
+import CarpetCleaning from "./pages/Services-Pages/Carpet-Cleaning";
+import CommercialCleaning from "./pages/Services-Pages/Commercial-Cleaning";
+import JanitorialServices from "./pages/Services-Pages/Janitorial-Services";
+import OfficeCleaning from "./pages/Services-Pages/Office-Cleaning";
 import Footer from "./components/Footer";    
     
 export default function App() {
   return (
-    <>
-      <h1>
-        This works!
-      </h1>
-      {/* <Routes>
+    <>      
+      <Routes>
         <Route path="/" element={<Header />}>
           <Route index element={<Landing />} />
           <Route path="/about" element={<About />} />
@@ -27,7 +24,7 @@ export default function App() {
           <Route path="/Janitorial-Services" element={<JanitorialServices />} />
           <Route path="/Office-Cleaning" element={<OfficeCleaning />} />
         </Route>
-      </Routes> */}
+      </Routes>
       <Footer />
     </>
   );
